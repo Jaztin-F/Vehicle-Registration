f8532b52d1fcd87546eab10d0be276e2ed3da597
Justine Francisco <131416285+Jaztin-F@users.noreply.github.com>
2023-12-19 19:28:39 +0800

    Add files via upload

---
+++ OOPFinal/src/OOP/Register.java
@@ -0,0 +1,14 @@
+//Register Class
+package OOP;
+
+public abstract class Register {
+
+    public abstract void register();
+
+    public abstract void read();
+    
+     public abstract void search();
+     
+     
+
+}
